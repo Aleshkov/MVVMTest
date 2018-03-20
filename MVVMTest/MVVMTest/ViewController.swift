3dcf7f59492fcf30c7c649316943e5a8e2f18360
andreyAleshkov <aleshkov70@yandex.ru>
2018-03-20 10:41:57 +0700

    initial commit

---
+++ MVVMTest/MVVMTest/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  MVVMTest
+//
+//  Created by andrey on 20/03/2018.
+//  Copyright © 2018 andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
